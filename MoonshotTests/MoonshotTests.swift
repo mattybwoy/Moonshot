20f10c13bac108fb2376abb977a9d597f83c03d4
Matthew Lock <matthew_lock20@hotmail.com>
2022-01-28 18:02:54 +0000

    Second test written testing changed currency makes successful call

--- MoonshotTests/MoonshotTests.swift
+++ MoonshotTests/MoonshotTests.swift
@@ -20,13 +20,26 @@ class MoonshotTests: XCTestCase {
         sut = nil
     }
 
-    func test_loadCoins_WhenSucessfulResponse_ReturnsSuccess() throws {
+    func test_loadCoins_WhenSuccessfulResponse_ReturnsSuccess() throws {
         //Arrange
         let expectation = self.expectation(description: "Loading Initial Cryptocurrency in USD")
         
         //Act
         sut.loadCoins {
             XCTAssertNotNil(self.sut.coins)
+            expectation.fulfill()
+        }
+        //Assert
+        self.wait(for: [expectation], timeout: 5)
+    }
+    
+    func test_loadCoins_WhenSuccessfulResponse_ReturnsRatesInGBP() {
+        //Arrange
+        let expectation = self.expectation(description: "Loads Cryptocurrency in GBP")
+        //Act
+        sut.loadCoins(currency: "gbp") {
+            XCTAssertNotNil(self.sut.coins)
+            XCTAssertEqual(self.sut.currentCurrency, "gbp")
             expectation.fulfill()
         }
         //Assert
