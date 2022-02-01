40276d39dffc1e1e3fb6195398d855c9c3ba8dfd
Matthew Lock <matthew_lock20@hotmail.com>
2022-02-01 17:02:15 +0000

    scrollCoin test written and mocked

--- MoonshotTests/MoonshotTests.swift
+++ MoonshotTests/MoonshotTests.swift
@@ -22,52 +22,45 @@ class MoonshotTests: XCTestCase {
     override func tearDownWithError() throws {
         sut = nil
     }
+    
+    func test_APIStatus_WhenSuccessfulResponse_ReturnsSuccess() {
+        
+        let jsonString = "{\"gecko_says\":\"(V3) To the Moon!\"}"
+        MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
+        let expectation = self.expectation(description: "Testing API Status")
+        
+        sut.testAPIStatus { (Status) in
+            XCTAssertEqual(Status?.gecko_says, "(V3) To the Moon!")
+            expectation.fulfill()
+        }
+        
+        self.wait(for: [expectation], timeout: 3)
+    }
 
     func test_loadCoins_WhenSuccessfulResponse_ReturnsSuccessInUSD() throws {
 
-        //Arrange
         let expectation = self.expectation(description: "Loading Initial Cryptocurrency in USD")
         let current_price = 38346
         let jsonString = "[{\"name\": \"Bitcoin\", \"id\": \"bitcoin\", \"current_price\": \(current_price)}]"
         MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
 
-        //Act
         sut.loadCoins { (_: [Coins]?) in
             XCTAssertEqual(self.sut.coins![0].name, "Bitcoin")
             XCTAssertEqual(self.sut.coins![0].id, "bitcoin")
             XCTAssertEqual(self.sut.coins![0].current_price, 38346)
             expectation.fulfill()
         }
-        //Assert
-        self.wait(for: [expectation], timeout: 3)
-    }
-    
-    func test_APIStatus_WhenSuccessfulResponse_ReturnsSuccess() {
         
-        //Arrange
-        let jsonString = "{\"gecko_says\":\"(V3) To the Moon!\"}"
-        MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
-        let expectation = self.expectation(description: "Testing API Status")
-        
-        //Act
-        sut.testAPIStatus { (Status) in
-            XCTAssertEqual(Status?.gecko_says, "(V3) To the Moon!")
-            expectation.fulfill()
-        }
-        
-        //Assert
         self.wait(for: [expectation], timeout: 3)
     }
     
     func test_loadCoins_WhenSuccessfulResponse_ReturnsRatesInGBP() {
         
-        //Arrange
         let expectation = self.expectation(description: "Loads Cryptocurrency in GBP")
         let current_price = 28794
         let jsonString = "[{\"name\": \"Bitcoin\", \"id\": \"bitcoin\", \"current_price\": \(current_price)}]"
         MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
         
-        //Act
         sut.loadCoins(currency: "gbp") { (_: [Coins]?) in
             XCTAssertEqual(self.sut.currentCurrency, "gbp")
             XCTAssertEqual(self.sut.coins![0].name, "Bitcoin")
@@ -76,8 +69,21 @@ class MoonshotTests: XCTestCase {
             expectation.fulfill()
         }
         
-        //Assert
         self.wait(for: [expectation], timeout: 3)
     }
 
+    func test_scrollCoin_WhenSuccessfulResponse_ReturnsNextPageOfResults() {
+        
+        let expectation = self.expectation(description: "Loads results of next page in current currency")
+        let current_price = 38346
+        let jsonString = "[{\"name\": \"Bitcoin\", \"id\": \"bitcoin\", \"current_price\": \(current_price)}]"
+        MockURLProtocol.stubResponseData = jsonString.data(using: .utf8)
+        sut.currentCurrency = "usd"
+        
+        sut.scrollCoin(pagination: true) {
+            expectation.fulfill()
+        }
+        XCTAssertEqual(sut.pageCount, 3)
+        self.wait(for: [expectation], timeout: 3)
+    }
 }
